e2947977e3fa8225d1e38826a4e3bb9ab2e78158
Raissa Pimentel <raissabmpimentel@gmail.com>
2020-05-16 21:52:41 -0300

    Setting better parameters ortographic camera

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 // Definição da cena, câmera (perspectiva) e renderizador
-
+var viewSize = 8;
+var aspectRatio = window.innerWidth / window.innerHeight;
 var scene = new THREE.Scene(); 
 //var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000); 
-var camera = new THREE.OrthographicCamera( window.innerWidth / - 250, window.innerWidth / 250, window.innerHeight / 250, window.innerHeight / - 250, 1, 1000 );
+var camera = new THREE.OrthographicCamera(-aspectRatio * viewSize / 2, aspectRatio * viewSize / 2, viewSize / 2, -viewSize / 2, 0.1, 1000);
 var renderer = new THREE.WebGLRenderer({
 canvas:document.getElementById("mycanvas"),
 alpha:true,
@@ -290,10 +291,11 @@ function animate() {
 // Reajustar proporção com a mudança do tamanho da tela
 function onWindowResize() {
     //camera.aspect = window.innerWidth / window.innerHeight;
-    camera.left = -window.innerWidth / 250;
-    camera.right = window.innerWidth / 250;
-    camera.top = window.innerHeight / 250;
-    camera.bottom = -window.innerHeight / 250;
+    var aspect = window.innerWidth / window.innerHeight;
+    camera.left = -aspect * viewSize / 2;
+    camera.right = aspect * viewSize  / 2;
+    camera.top = viewSize / 2;
+    camera.bottom = -viewSize / 2;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
 }
